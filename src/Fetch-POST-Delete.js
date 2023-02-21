b81ed76deab3a5aaf6782c4824327ab339e4aff6
yapei <yapei@redhat.com>
2023-02-21 10:51:19 +0800

    add first change

---
+++ src/Fetch-POST-Delete.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+   const [title, setTitle] = useState('');
+   const [body, setBody] = useState('');
+   const [posts, setPosts] = useState([]);
+
+   // fetch API 中的 GET 方法
+   useEffect(() => {
+      const fetchPost = async () => {
+         const response = await fetch(
+            'https://jsonplaceholder.typicode.com/posts?_limit=10'
+         );
+         const data = await response.json();
+         console.log(data);
+         setPosts(data);
+      };
+      fetchPost();
+   }, []);
+
+   //fetch API 中的 DELETE 方法
+   const deletePost = async (id) => {
+      let response = await fetch(
+         `https://jsonplaceholder.typicode.com/posts/${id}`,
+         {
+            method: 'DELETE',
+         }
+      );
+      if (response.status === 200) {
+         setPosts(
+            posts.filter((post) => {
+               return post.id !== id;
+            })
+         );
+      } else {
+         return;
+      }
+   };
+
+   // fetch API 中的 POST 方法
+   const addPosts = async (title, body) => {
+      let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+         method: 'POST',
+         body: JSON.stringify({
+            title: title,
+            body: body,
+            userId: Math.random().toString(36).slice(2),
+         }),
+         headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+         },
+      });
+      let data = await response.json();
+      setPosts((posts) => [data, ...posts]);
+      setTitle('');
+      setBody('');
+   };
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      addPosts(title, body);
+   };
+
+   return (
+      <div className="app">
+         <div className="add-post-container">
+            <form onSubmit={handleSubmit}>
+               <input type="text" className="form-control" value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+               />
+               <textarea name="" className="form-control" id="" cols="10" rows="8" 
+                  value={body} onChange={(e) => setBody(e.target.value)} 
+               ></textarea>
+               <button type="submit">Add Post</button>
+            </form>
+         </div>
+         {/* ... */}
+      </div>
+   );
+};
+
+export default App;
